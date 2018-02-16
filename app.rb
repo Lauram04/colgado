d3c61b7d8465d22d71798d8118a583f1a30ad5ce
kleer <kleer@kleer-csd-box>
2018-02-16 15:53:15 -0500

    fix

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require './lib/colgado.rb'
 get '/' do
 	session["contador"] = 0
 	juego = Colgado.new
+	session["letra"] = juego
 	session["longitud"] = juego.longitud
 	session["lineas"] = juego.lineas
 	erb :colgado
@@ -12,14 +13,14 @@ end
 
 post '/validar' do
 	letra = session["letra"]
-	
 	if letra.evaluar(params["campo"])=="Esta"
+		session["lineas"] = letra.sustituye(params["campo"])
 		session["validacion"]="Esta"
 	else
 		session["validacion"]="No esta"
 	end
 	
-    if letra.resultado == true
+    if letra.resultado
     	erb :resultado
     else
     	erb :colgado
--- features/resultado.feature
+++ features/resultado.feature
@@ -25,7 +25,7 @@ Feature:
 	And Presiono "validar"
 	And Inserto una "a" en "campo"
 	And Presiono "validar"
-	Then debo ver "la palabra neta es un modismo mexicano que remplaza a la palabra verdad"
+	Then debo ver "La palabra neta es un modismo mexicano que remplaza a la palabra verdad."
 
 Scenario: mostrar una frase que contenga la palabra
 	Given inicio el juego
